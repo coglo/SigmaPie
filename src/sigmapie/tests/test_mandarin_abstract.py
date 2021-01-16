416c45e1f7432f23290840eaec661ba734da462b
yangliustonybrook <yang.liu.11@stonybrook.edu>
2021-01-15 20:59:49 -0500

    updated the mandarin tests

--- src/sigmapie/tests/test_mandarin_abstract.py
+++ src/sigmapie/tests/test_mandarin_abstract.py
@@ -14,8 +14,9 @@ from sigmapie.sp_class import SP
 from sigmapie.sl_class import SL
 from sigmapie.tsl_class import TSL
 from sigmapie.mtsl_class import MTSL
+import pytest
 
-def evaluate_models(evaluator, data):
+def evaluate_models(evaluator, data, tsl=True):
     
     sp_h = SP()
     sl_h = SL()
@@ -40,21 +41,22 @@ def evaluate_models(evaluator, data):
     evaluator(data)
     evaluator(sp_h.generate_sample(n=1000, repeat=True))
     evaluator(sl_h.generate_sample(n=1000, repeat=True))
-    evaluator(tsl_h.generate_sample(n=1000, repeat=True))
+    if tsl:
+        evaluator(tsl_h.generate_sample(n=1000, repeat=True))
     evaluator(mtsl_h.generate_sample(n=1000, repeat=True))
     
     print("SL k:", sl_h.k)
-
     print("SL sample:", sl_h.generate_sample(20, repeat=False), "\n")
     print("SP sample:", sp_h.generate_sample(20, repeat=False), "\n")
-    print("TSL tier:", tsl_h.tier)
-    print("TSL sample:", tsl_h.generate_sample(20, repeat=False), "\n")
+    if tsl:
+        print("TSL tier:", tsl_h.tier)
+        print("TSL sample:", tsl_h.generate_sample(20, repeat=False), "\n")
     print("MTSL tiers:", mtsl_h.tier)
     print("MTSL sample:", mtsl_h.generate_sample(20, repeat=False))
-
     print("SP alphabet:", sp_h.alphabet)
     print("SL alphabet:", sl_h.alphabet)
-    print("TSL alphabet:", tsl_h.alphabet)
+    if tsl:
+        print("TSL alphabet:", tsl_h.alphabet)
     print("MTSL alphabet:", mtsl_h.alphabet)
     print("SP alphabet:", sp_h.grammar)
     print("SL alphabet:", sl_h.grammar)
@@ -62,7 +64,8 @@ def evaluate_models(evaluator, data):
     print("MTSL alphabet:", mtsl_h.grammar)
     print("SP alphabet:", sp_h.check_polarity())
     print("SL alphabet:", sl_h.check_polarity())
-    print("TSL alphabet:", tsl_h.check_polarity())
+    if tsl:
+        print("TSL alphabet:", tsl_h.check_polarity())
     print("MTSL alphabet:", mtsl_h.check_polarity())
     print("MTSL tiers:", mtsl_h.tier)
 
@@ -76,192 +79,18 @@ def test_no_high_high():
     evaluate_models(evaluate_nhh_words, generate_nhh(n=1000))
 
 def test_no_vc():
-
-    data = generate_nvc(n=1000)
-
-    sp_h = SP()
-    sl_h = SL()
-    tsl_h = TSL()
-    mtsl_h = MTSL()
-
-    sp_h.data = data
-    sl_h.data = data
-    tsl_h.data = data
-    mtsl_h.data = data
-    
-    sp_h.extract_alphabet()
-    sl_h.extract_alphabet()
-    tsl_h.extract_alphabet()
-    mtsl_h.extract_alphabet()
-    
-    sp_h.learn()
-    sl_h.learn()
-    tsl_h.learn()
-    mtsl_h.learn()
-
-    evaluate_nvc_words(data)
-    evaluate_nvc_words(sp_h.generate_sample(n=1000, repeat=True))
-    evaluate_nvc_words(sl_h.generate_sample(n=1000, repeat=True))
-    #evaluate_nvc_words(tsl_h.generate_sample(n=1000, repeat=True))
-    evaluate_nvc_words(mtsl_h.generate_sample(n=1000, repeat=True))
-    print("SP alphabet:", sp_h.alphabet)
-    print("SL alphabet:", sl_h.alphabet)
-    print("TSL alphabet:", tsl_h.alphabet)
-    print("MTSL alphabet:", mtsl_h.alphabet)
-    print("SP alphabet:", sp_h.grammar)
-    print("SL alphabet:", sl_h.grammar)
-    print("TSL alphabet:", tsl_h.grammar)
-    print("MTSL alphabet:", mtsl_h.grammar)
-    print("SP alphabet:", sp_h.check_polarity())
-    print("SL alphabet:", sl_h.check_polarity())
-    print("TSL alphabet:", tsl_h.check_polarity())
-    print("MTSL alphabet:", mtsl_h.check_polarity())
-    print("MTSL tiers:", mtsl_h.tier)
+    evaluate_models(evaluate_nvc_words, generate_nvc(n=1000))
 
 def test_schwa_roundness():
+    evaluate_models(evaluate_sro_words, generate_nvc(n=1000), tsl=False)
 
-    data = generate_sro(n=1000)
-
-    sp_h = SP()
-    sl_h = SL()
-    tsl_h = TSL()
-    mtsl_h = MTSL()
-
-    sp_h.data = data
-    sl_h.data = data
-    tsl_h.data = data
-    mtsl_h.data = data
-    
-    sp_h.extract_alphabet()
-    sl_h.extract_alphabet()
-    tsl_h.extract_alphabet()
-    mtsl_h.extract_alphabet()
-    
-    sp_h.learn()
-    sl_h.learn()
-    tsl_h.learn()
-    mtsl_h.learn()
-
-    evaluate_sro_words(data)
-    evaluate_sro_words(sp_h.generate_sample(n=1000, repeat=True))
-    evaluate_sro_words(sl_h.generate_sample(n=1000, repeat=True))
-    #evaluate_sro_words(tsl_h.generate_sample(n=1000, repeat=True))
-    evaluate_sro_words(mtsl_h.generate_sample(n=1000, repeat=True))
-    print("SP alphabet:", sp_h.alphabet)
-    print("SL alphabet:", sl_h.alphabet)
-    print("TSL alphabet:", tsl_h.alphabet)
-    print("MTSL alphabet:", mtsl_h.alphabet)
-    print("SP alphabet:", sp_h.grammar)
-    print("SL alphabet:", sl_h.grammar)
-    print("TSL alphabet:", tsl_h.grammar)
-    print("MTSL alphabet:", mtsl_h.grammar)
-    print("SP alphabet:", sp_h.check_polarity())
-    print("SL alphabet:", sl_h.check_polarity())
-    print("TSL alphabet:", tsl_h.check_polarity())
-    print("MTSL alphabet:", mtsl_h.check_polarity())
-    print("MTSL tiers:", mtsl_h.tier)
-'''
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_schwa_roundness_io():
+    evaluate_models(evaluate_sro_io, generate_sro_io(n=1000))
 
-    data = generate_sro_io(n=1000)
-
-    sp_h = SP()
-    sl_h = SL()
-    tsl_h = TSL()
-    mtsl_h = MTSL()
-
-    sp_h.data = data
-    sl_h.data = data
-    tsl_h.data = data
-    mtsl_h.data = data
-    
-    sp_h.extract_alphabet()
-    sl_h.extract_alphabet()
-    tsl_h.extract_alphabet()
-    mtsl_h.extract_alphabet()
-    
-    sp_h.learn()
-    sl_h.learn()
-    tsl_h.learn()
-    mtsl_h.learn()
-
-    evaluate_sro_io(data)
-    evaluate_sro_io(sp_h.generate_sample(n=1000, repeat=True))
-    evaluate_sro_io(sl_h.generate_sample(n=1000, repeat=True))
-    evaluate_sro_io(tsl_h.generate_sample(n=1000, repeat=True))
-    evaluate_sro_io(mtsl_h.generate_sample(n=1000, repeat=True))
-'''
 def test_vowel_frontness():
+    evaluate_models(evaluate_vfr_words, generate_vfr(n=1000))
 
-    data = generate_vfr(n=1000)
-
-    sp_h = SP()
-    sl_h = SL()
-    tsl_h = TSL()
-    mtsl_h = MTSL()
-
-    sp_h.data = data
-    sl_h.data = data
-    tsl_h.data = data
-    mtsl_h.data = data
-    
-    sp_h.extract_alphabet()
-    sl_h.extract_alphabet()
-    tsl_h.extract_alphabet()
-    mtsl_h.extract_alphabet()
-    
-    sp_h.learn()
-    sl_h.learn()
-    tsl_h.learn()
-    mtsl_h.learn()
-
-    evaluate_vfr_words(data)
-    evaluate_vfr_words(sp_h.generate_sample(n=1000, repeat=True))
-    evaluate_vfr_words(sl_h.generate_sample(n=1000, repeat=True))
-    #evaluate_vfr_words(tsl_h.generate_sample(n=1000, repeat=True))
-    evaluate_vfr_words(mtsl_h.generate_sample(n=1000, repeat=True))
-    print("SP alphabet:", sp_h.alphabet)
-    print("SL alphabet:", sl_h.alphabet)
-    print("TSL alphabet:", tsl_h.alphabet)
-    print("MTSL alphabet:", mtsl_h.alphabet)
-    print("SP alphabet:", sp_h.grammar)
-    print("SL alphabet:", sl_h.grammar)
-    print("TSL alphabet:", tsl_h.grammar)
-    print("MTSL alphabet:", mtsl_h.grammar)
-    print("SP alphabet:", sp_h.check_polarity())
-    print("SL alphabet:", sl_h.check_polarity())
-    print("TSL alphabet:", tsl_h.check_polarity())
-    print("MTSL alphabet:", mtsl_h.check_polarity())
-    print("MTSL tiers:", mtsl_h.tier)
-
-'''
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_vowel_frontness_io():
-
-    data = generate_vfr_io(n=1000)
-
-    sp_h = SP()
-    sl_h = SL()
-    tsl_h = TSL()
-    mtsl_h = MTSL()
-
-    sp_h.data = data
-    sl_h.data = data
-    tsl_h.data = data
-    mtsl_h.data = data
-    
-    sp_h.extract_alphabet()
-    sl_h.extract_alphabet()
-    tsl_h.extract_alphabet()
-    mtsl_h.extract_alphabet()
-    
-    sp_h.learn()
-    sl_h.learn()
-    tsl_h.learn()
-    mtsl_h.learn()
-
-    evaluate_vfr_io(data)
-    evaluate_vfr_io(sp_h.generate_sample(n=1000, repeat=True))
-    evaluate_vfr_io(sl_h.generate_sample(n=1000, repeat=True))
-    evaluate_vfr_io(tsl_h.generate_sample(n=1000, repeat=True))
-    evaluate_vfr_io(mtsl_h.generate_sample(n=1000, repeat=True))
-'''
+    evaluate_models(evaluate_vfr_io, generate_vfr_io(n=1000))
